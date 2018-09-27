7eb71d73d57e5010644e722a4016f7f5d8495f95
vardhan26 <vardhan2605@gmail.com>
2018-09-27 13:37:52 +0530

    initial commit

---
+++ mail.php
@@ -0,0 +1,31 @@
+<?php
+    $to = 'igvc.iitk@gmail.com';
+    $firstname = $_POST["fname"];
+    $subject= $_POST["subject"];
+    $email= $_POST["email"];
+    $text= $_POST["message"];
+    
+
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'.$firstname.'  '.$lastname.'</td>
+        </tr>
+        <tr><td>Subject: '.$subject.'</td></tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>Email: '.$text.'</td></tr>
+        
+    </table>';
+
+    if (@mail($to, $email, $message, $headers))
+    {
+        echo 'The message has been sent.';
+    }else{
+        echo 'failed';
+    }
+
+?>
