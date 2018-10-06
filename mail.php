48dd5dc4a96a8b617f4ee71b215dce3e3ec8ed49
vardhan26 <vardhan2605@gmail.com>
2018-10-06 14:12:35 +0530

    changed @mail

--- mail.php
+++ mail.php
@@ -21,7 +21,7 @@
         
     </table>';
 
-    if (@mail($to, $email, $message, $headers))
+    if (@mail($to, $subject, $message, $headers))
     {
         echo 'The message has been sent.';
     }else{
